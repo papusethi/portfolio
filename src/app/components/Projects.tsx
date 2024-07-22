4ae3281f2805b5062c4e62646412690a5a8396af
Papu Sethi <mrpapur2001@gmail.com>
2024-07-22 23:02:51 +0530

    Completed updating of projects section

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThumbnailProjectFour, ThumbnailProjectOne, ThumbnailProjectThree, ThumbnailProjectTwo } from '../assets/img';
+import { ThumbnailProjectOne } from '../assets/img';
 
 const Projects: React.FC = () => {
   return (
@@ -7,52 +7,34 @@ const Projects: React.FC = () => {
       <h2 className="section__title">Projects</h2>
       <span className="section__subtitle">Most recent work</span>
 
-      <div className="projects__container container grid section__border">
+      <div className="projects__container container section__border">
         {/* ==================== PROJECTS 1 ==================== */}
-        <div className="projects__content swiper-slide">
-          <img src={ThumbnailProjectOne} alt="project" className="projects__img" />
+        <div className="projects__content">
           <div>
-            <span className="projects__subtitle">Web</span>
-            <h1 className="projects__title">Modern Website</h1>
-            <a href="/#" className="projects__button">
-              View Demo <i className="ri-arrow-right-line" />
-            </a>
+            <img src={ThumbnailProjectOne} alt="project" className="projects__img" />
+            <div>
+              <span className="projects__subtitle">Web Application</span>
+              <h1 className="projects__title">Supply Chain Management</h1>
+              <a href="https://blueyonder.com/" className="projects__button">
+                Visit site <i className="ri-arrow-right-line" />
+              </a>
+            </div>
           </div>
-        </div>
-
-        {/* ==================== PROJECTS 2 ==================== */}
-        <div className="projects__content swiper-slide">
-          <img src={ThumbnailProjectTwo} alt="project" className="projects__img" />
           <div>
-            <span className="projects__subtitle">Web</span>
-            <h1 className="projects__title">ECommerce Store</h1>
-            <a href="/#" className="projects__button">
-              View Demo <i className="ri-arrow-right-line" />
-            </a>
-          </div>
-        </div>
-
-        {/* ==================== PROJECTS 3 ==================== */}
-        <div className="projects__content swiper-slide">
-          <img src={ThumbnailProjectThree} alt="project" className="projects__img" />
-          <div>
-            <span className="projects__subtitle">Design</span>
-            <h1 className="projects__title">Application Design</h1>
-            <a href="/#" className="projects__button">
-              View Demo <i className="ri-arrow-right-line" />
-            </a>
-          </div>
-        </div>
-
-        {/* ==================== PROJECTS 4 ==================== */}
-        <div className="projects__content swiper-slide">
-          <img src={ThumbnailProjectFour} alt="project" className="projects__img" />
-          <div>
-            <span className="projects__subtitle">Animation</span>
-            <h1 className="projects__title">Animation Prototypes</h1>
-            <a href="/#" className="projects__button">
-              View Demo <i className="ri-arrow-right-line" />
-            </a>
+            <ul className="projects__description" style={{ listStyleType: 'initial' }}>
+              <li>
+                A specialized SaaS platform tailored for business planning architects, focusing on advanced Supply Chain
+                Management (SCM).
+              </li>
+              <li>
+                Led the development of a comprehensive Supply Chain Management (SCM) SaaS application from inception to
+                multiple release cut deployments.
+              </li>
+              <li>
+                Implemented solutions to eﬀiciently handle large datasets and concurrent operations, ensuring optimal
+                performance and user experience under varying workloads.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
