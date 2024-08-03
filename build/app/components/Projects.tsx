039dc67ec81b940b16f8d355d8093389deffa39c
Papu Sethi <mrpapur2001@gmail.com>
2024-08-03 22:44:16 +0530

    Updates

--- build/app/components/Projects.tsx
+++ build/app/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThumbnailProjectOne } from '../assets/img';
+import { ThumbnailProjectOne, ThumbnailProjectTwo } from '../assets/img';
 
 const Projects: React.FC = () => {
   return (
@@ -31,9 +31,44 @@ const Projects: React.FC = () => {
                 multiple release cut deployments.
               </li>
               <li>
-                Implemented solutions to eﬀiciently handle large datasets and concurrent operations, ensuring optimal
+                Implemented solutions to efficiently handle large datasets and concurrent operations, ensuring optimal
                 performance and user experience under varying workloads.
               </li>
+              <li>Technologies used: React, TypeScript, Material UI, Jest, Testing Library, Redux Toolkit.</li>
+            </ul>
+          </div>
+        </div>
+
+        {/* ==================== PROJECTS 2 ==================== */}
+
+        <div className="projects__content">
+          <div>
+            <img src={ThumbnailProjectTwo} alt="project" className="projects__img" />
+            <div>
+              <span className="projects__subtitle">Web Application</span>
+              <h1 className="projects__title">QuikNotes</h1>
+              {/* <a href="https://blueyonder.com/" className="projects__button" target="_blank" rel="noreferrer">
+                Visit site <i className="ri-arrow-right-line" />
+              </a> */}
+            </div>
+          </div>
+          <div>
+            <ul className="projects__description" style={{ listStyleType: 'initial' }}>
+              <li>
+                A powerful SaaS application designed to capture and organize your ideas and thoughts effortlessly. With
+                intuitive features for note-taking, you can easily store, manage, and access your insights anytime,
+                anywhere.
+              </li>
+              <li>
+                Designed and implemented a Back-end for Front-end (BFF) architecture using Express.js and MongoDB to
+                streamline data flow and improve application performance.
+              </li>
+              <li>
+                Developed robust user authentication systems to secure private routes, ensuring a seamless and secure
+                user experience. Additionally, created customizable pages tailored to various industry applications,
+                enhancing the applications versatility and user engagement.
+              </li>
+              <li>Technologies used: React, TypeScript, Material UI, Redux Toolkit, Express.js, MongoDB.</li>
             </ul>
           </div>
         </div>
