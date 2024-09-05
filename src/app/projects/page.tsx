aee951fbfd0980bbc2d823edc2509bc5554f557f
Papu Sethi <mrpapur2001@gmail.com>
2024-09-05 23:02:59 +0530

    Completed projects components

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -43,45 +43,48 @@ export const metadata: Metadata = {
 
 const Projects = () => {
   return (
-    <div className="px-8 pt-10 pb-16 relative">
-      <div className="w-4/6">
-        <h1 className="text-4xl font-bold text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          Things I’ve made trying to put my dent in the universe.
-        </h1>
-        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400 leading-relaxed">
-          I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of
-          them are open-source, so if you see something that piques your interest, check out the code and contribute if
-          you have ideas for how it can be improved.
-        </p>
-      </div>
+    <div className="sm:px-8 my-16 sm:my-32">
+      <div className="mx-auto w-full max-w-7xl lg:px-8">
+        <div className="relative px-4 sm:px-8 lg:px-12">
+          {/* start editing from here */}
+          {/* start header and desc */}
+          <div className="max-w-2xl">
+            <h1 className="text-4xl font-bold sm:text-5xl text-zinc-800 dark:text-zinc-100">
+              Things I’ve made trying to put my dent in the universe.
+            </h1>
+            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many
+              of them are open-source, so if you see something that piques your interest, check out the code and
+              contribute if you have ideas for how it can be improved.
+            </p>
+          </div>
+          {/* end header and desc */}
 
-      <div className="mt-16 -mx-6">
-        <div className="grid grid-cols-3 gap-4">
-          {projectsConfig.map(({ favicon, name, description, githubLink }) => (
-            <div key={name} className="flex flex-col gap-4 p-6 rounded-xl hover:bg-zinc-50">
-              <div>
-                <span className="inline-block rounded-full border p-1 ring-1 ring-zinc-100">
-                  <Image src={favicon} alt={name} width={32} height={32} />
-                </span>
-              </div>
+          <div className="mt-16 sm:mt-20">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+              {projectsConfig.map(({ favicon, name, description, githubLink }) => (
+                <li
+                  key={name}
+                  className="group relative trasition flex flex-col items-start rounded-xl p-6 hover:bg-zinc-100 dark:hover:bg-zinc-900/50"
+                >
+                  <div className="w-12 h-12 flex items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+                    <Image src={favicon} alt={name} width={32} height={32} />
+                  </div>
 
-              <div>
-                <h1 className="text-base font-semibold text-zinc-800 dark:text-zinc-100">{name}</h1>
-                <p className="text-sm text-zinc-600 dark:text-zinc-400 mt-3 leading-relaxed">{description}</p>
-              </div>
+                  <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">{name}</h2>
+                  <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">{description}</p>
 
-              <div className="mt-2 text-sm text-zinc-400 dark:text-zinc-500 flex gap-2 items-center hover:text-teal-500">
-                <span>
-                  <LinkIcon size={16} />
-                </span>
-                <Link href={githubLink}>{githubLink}</Link>
-              </div>
-            </div>
-          ))}
+                  <div className="mt-6 flex items-center gap-2 text-sm text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                    <LinkIcon size={16} />
+                    <Link href={githubLink}>{githubLink}</Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* end editing till here */}
         </div>
       </div>
-
-      <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>
     </div>
   );
 };
